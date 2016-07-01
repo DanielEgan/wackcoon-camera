f22e510d5117f38f59b457c7c48880513a9bc621
Jeremy Foster <jeremy.foster@live.com>
2016-07-01 12:32:15 -0700

    auto detect board type

--- index.ts
+++ index.ts
@@ -1,58 +1,63 @@
 import * as path from 'path';
-import RaspiCam = require('raspicam');
-let ffmpeg = require('ffmpeg');
+import * as fs from 'fs';
 
 let imagesRoot = path.join(__dirname, '..', 'images');
 let FREQUENCY = 10;
+let boardType;
 
-// if ("pi") {
-//     let piCamera = new RaspiCam({
-//         mode: 'photo',
-//         timelapse: FREQUENCY * 1000,
-//         timeout: 0,
-//         rotation: 180,
-//         encoding: 'png',
-//         exposure: 'auto',
-//         saturation: 50,
-//         brightness: 50,
-//         quality: 90,
-//         thumb: 'none',
-//         output: path.join(imagesRoot, '%d.png'),
-//         awb: 'sun',
-//         width: 1296,
-//         height: 730,
-//         // preview:"0,0,200,200",
-//         nopreview: true
-//     });
+fs.readFile('/proc/cpuinfo', (err, data) => {
+    var cpuinfo = data.toString();
+    console.log(cpuinfo);
+    if (/ARMv7/.test(cpuinfo))
+        boardType = 'pi';
+    else if (/GenuineIntel/.test(cpuinfo))
+        boardType = 'edison';
+    else
+        boardType = 'unknown';
 
-//     //start taking timelapses
-//     console.log('starting camera...');
-//     piCamera.start();
-// }
-// else if("edison") {
-// let timer = setInterval(, FREQUENCY * 1000);
-try {
-    var process = new ffmpeg('/dev/video0');
+    switch (boardType) {
+        case 'pi':
+            let RaspiCam = require('raspicam');
+            console.log('found a pi');
+            let piCamera = new RaspiCam({
+                mode: 'photo',
+                timelapse: FREQUENCY * 1000,
+                timeout: 0,
+                rotation: 180,
+                encoding: 'png',
+                exposure: 'auto',
+                saturation: 50,
+                brightness: 50,
+                quality: 90,
+                thumb: 'none',
+                output: path.join(imagesRoot, '%d.png'),
+                awb: 'sun',
+                width: 1296,
+                height: 730,
+                // preview:"0,0,200,200",
+                nopreview: true
+            });
 
-    process.then(function (video) {
-        // Callback mode
-        video.fnExtractFrameToJPG('.', {
-            frame_rate: 1,
-            duration_time: 5,
-            size: '1280x720',
-            file_name: '%t.png'
-        }, function (error, files) {
-            console.log(error);
-            
-            if(files) console.log(files.length);
-            if (!error)
-                console.log('Frames: ' + files);
-        });
-    }, function (err) {
-        console.log('Error: ' + err);
-    });
-} catch (e) {
-    console.log(e.code);
-    console.log(e.msg);
-}
-// }
+            //start taking timelapses
+            console.log('starting camera...');
+            piCamera.start();
+            break;
+        case 'edison':
+            let ffmpeg = require('./ffmpeg');
+            console.log('found an edison');
+
+            new ffmpeg('/dev/video0').then(function (stream) {
+                // Callback mode
+                stream.fnExtractFrameToPNG('.', {
+                    frame_rate: 1,
+                    size: '1280x720',
+                    file_name: '%t.png'
+                });
+            });
+            console.log('starting camera...');
+            break;
+        default:
+            console.log('unknown board type');
+
+    }
+});
